9ae2e63b454be0f9dfd6772727b18151e1c10797
Imran Islam Hridoy <59428414+hridoyofficial3@users.noreply.github.com>
2026-09-21 01:54:43 +0600

    Delete init.js

--- init.js
+++
@@ -1,9 +0,0 @@
-/* ============================================================
-   Init
-   ============================================================ */
-document.getElementById('budgetTypeRow').style.display = curType==='expense' ? 'block' : 'none';
-
-loadData();
-applyDarkMode();
-applyLanguage();
-syncTransferToOptions();
