9bc5692707146c35252bace78e6aa23028bf2dfd
Imran Islam Hridoy <59428414+hridoyofficial3@users.noreply.github.com>
2026-09-24 19:59:56 +0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'hisab-khata-v59';
+const CACHE = 'hisab-khata-v60;
 const FILES = [
   './', './index.html', './style.css', './manifest.json',
   './icon-192.png', './icon-512.png', './icon-maskable-512.png',
